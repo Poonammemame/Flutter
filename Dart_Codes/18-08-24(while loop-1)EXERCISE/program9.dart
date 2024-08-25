8c9fca75a54d4fb0a8a4c83a1416fd02d4d73f17
Poonammemame <pmemane2612@gmail.com>
2024-08-25 09:55:22 +0530

    Exercise

---
+++ Dart_Codes/18-08-24(while loop-1)EXERCISE/program9.dart
@@ -0,0 +1,11 @@
+void main() {
+  int numDays = 7;
+  while (numDays >= 0) {
+    if (numDays == 0) {
+      print(" 0 days Assignment is Overdue");
+    } else {
+      print("$numDays days are remaining");
+    }
+    numDays--;
+  }
+}
