141fc9ce59ab879d79e55bcf5729e8fd0660e618
Poonammemame <pmemane2612@gmail.com>
2024-10-14 20:21:55 +0530

    Exercise

---
+++ Flutter App/Class Apps/Course Apps/QuizApp/list.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<Map> friends = [
+    {
+      "name": "Poonam",
+      "cname": "Infosys",
+    },
+    {
+      "name": "Disha",
+      "cname": "wipro",
+    }
+  ];
+  print(friends);
+  print(friends[1]);
+  print(friends[0]["name"]);
+}
